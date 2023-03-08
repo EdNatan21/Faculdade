cf898b4032d7c83c33e1671da4ac6afe3cff5d4b
EdNatan21 <90695383+EdNatan21@users.noreply.github.com>
2023-03-08 09:24:12 -0300

    Tabuada e app

--- JavaScript End/Aula02/Modulo/Tabuada.js
+++ JavaScript End/Aula02/Modulo/Tabuada.js
@@ -1,6 +1,42 @@
-/*
+/*----------------------------------------------------------
 ° Objetivo: Realizar o calculo de uma tabuada
 ° Autor: Natan
 ° Data: 07/02/2023
 ° Versão: 1.0
-*/
+----------------------------------------------------------*/
+
+//Gerar a tabuada de um número (function Call Back)
+const calcularTabuada = function(numeroTabuada){
+    //Entradas
+    let tabuada = numeroTabuada
+    let status
+    let cont = 0
+    let resultado
+
+    //Processamentos
+    if(tabuada == null)
+        status = false
+
+    else if(isNaN(tabuada))
+        status = false
+    
+    else if(tabuada == 0)
+        status = false
+
+    else{
+        //while
+        while (cont <= 10){
+            resultado = tabuada * cont
+            console.log(`${tabuada} x ${cont} = ${resultado}`)
+            cont = cont + 1
+        }
+        status = true
+    }
+    //Saída
+    return status;
+}
+
+//Para Acessar uma função em outro arquivo, é nescessário fazer o exports da função.
+module.exports = {
+    calcularTabuada
+}
---
+++ JavaScript End/Aula02/app.js
@@ -0,0 +1,13 @@
+//Iport dos arquievos e funções
+var readline = require('readline')
+var matematica = require('./Modulo/Tabuada')
+
+var entradaDados = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout 
+})
+
+entradaDados.question('Digite uma tabuada: ', function(numero){
+    let tabuada = numero
+    matematica.calcularTabuada(tabuada)
+})
